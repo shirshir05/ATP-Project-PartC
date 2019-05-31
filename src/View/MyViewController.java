e9147b5027e80b23c012eb13fb9bb07f105159fa
ortal <ortalprpr@gmail.com>
2019-05-31 16:44:51 +0300

    Merge branch 'shir' of https://github.com/shirshir05/ATP-Project-PartC into Ortal
    # Conflicts:
    #	src/View/MyView.fxml
    #	src/View/MyViewController.java

--- src/View/MyViewController.java
+++ src/View/MyViewController.java
@@ -1,26 +1,79 @@
 package View;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 import sun.audio.*;
 
 
-public class MyViewController implements IView{
-    public void mazeMouseClicked(MouseEvent mouseEvent) {
+public class MyViewController implements IView {
+
+    @FXML
+    public MazeDisplayer mazeDisplayer;
+
+    int[][] mazeData = { // a stub...
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1},
+            {0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1},
+            {1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1},
+            {1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1},
+            {1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1},
+            {1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1},
+            {1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1}
+    };
+
+
+    public void generateMaze() {
+        //int rows = Integer.valueOf(txtfld_rowsNum.getText());
+        //int columns = Integer.valueOf(txtfld_columnsNum.getText());
+        //this.mazeDisplayer.setMaze(getRandomMaze(rows,columns));
+        this.mazeDisplayer.setMaze(mazeData);
     }
 
+    public void KeyPressed(KeyEvent keyEvent) {
+        int characterRowCurrentPosition = mazeDisplayer.getCharacterPositionRow();
+        int characterColumnCurrentPosition = mazeDisplayer.getCharacterPositionColumn();
+        int characterRowNewPosition = characterRowCurrentPosition;
+        int characterColumnNewPosition = characterColumnCurrentPosition;
 
+        if (keyEvent.getCode() == KeyCode.UP) {
+            characterRowNewPosition = characterRowCurrentPosition - 1;
+            characterColumnNewPosition = characterColumnCurrentPosition;
+        } else if (keyEvent.getCode() == KeyCode.DOWN) {
+            characterRowNewPosition = characterRowCurrentPosition + 1;
+            characterColumnNewPosition = characterColumnCurrentPosition;
+        } else if (keyEvent.getCode() == KeyCode.RIGHT) {
+            characterRowNewPosition = characterRowCurrentPosition;
+            characterColumnNewPosition = characterColumnCurrentPosition + 1;
+        } else if (keyEvent.getCode() == KeyCode.LEFT) {
+            characterRowNewPosition = characterRowCurrentPosition;
+            characterColumnNewPosition = characterColumnCurrentPosition - 1;
+        } else if (keyEvent.getCode() == KeyCode.HOME) {
+            characterRowNewPosition = 0;
+            characterColumnNewPosition = 0;
+        }
 
+        //Updates the MazeDisplayer
+        mazeDisplayer.setCharacterPosition(characterRowNewPosition, characterColumnNewPosition);
 
-    public void music()
-    {
+        keyEvent.consume();
+    }
+
+    //Prevent the focus taking problem of the TextFields
+    public void mazeMouseClicked(MouseEvent mouseEvent) {
+        mazeDisplayer.requestFocus();
+    }
+
+    public void music() {
         AudioPlayer MGP = AudioPlayer.player;
         AudioStream BGM;
         AudioData MD;
 
     }
-
-
 
 
 }
